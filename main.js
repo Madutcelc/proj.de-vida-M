7af356595298b0217bd648e535506d7a2d86e9cb
Madutcelc <142222925+Madutcelc@users.noreply.github.com>
2024-03-24 11:41:53 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+const botoes =
+  document.querySelectorAll(«.botao»);
+console.log(botoes)
+  for(let i=0;i <botoes.length;i++)
+  { botoes[i].onclick = function() {
+    for(let j=0;j<botoes.length;j++)
+      { botoes[j].classList.remove(«ativo»);
+      }
+    botoes[i].classList.add(«ativo»);
+  }
+    const textos =
+      document.querySelectorAll(“.aba-conteudo”);
+   textos[i].classList.add(“ativo”);
+   textos[j].classList.remove(“ativo”);
+  } 
+const contadores =
+  document.querySelectorAll(“.contador”);
+
+const contadores =
+  document.querySelectorAll(“.contador”);
+contadores[0]
+contadores[0].textContent =
+  “Contagem regressiva”;
+
+const contadores = 
+  document.querySelectorAll(“.contador”); 
+
+const tempoObjetivo1 =
+  new Date(“2024-10-05”); 
+
+const contadores =  document.querySelectorAll(“.contador”);
+
+const tempoObjetivo1 = new Date(“2024-10-05”); 
+contadores[0].textContent = tempoObjetivo1;
